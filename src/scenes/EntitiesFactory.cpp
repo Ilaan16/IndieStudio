baba415b7ac5eebb69bd02616935d65f1f5e467f
Ilan <ilan.chekroun@epitech.eu>
2022-06-13 14:41:59 +0200

    [EDIT] - Fix Text & positions SceneMenu

--- src/scenes/EntitiesFactory.cpp
+++ src/scenes/EntitiesFactory.cpp
@@ -24,7 +24,7 @@ namespace indie {
 
     void EntitiesFactory::createButton(std::map<typeEntity ,std::vector<std::shared_ptr<Entity>>> &_entities, std::string text, Vector3D pos, Vector3D rect, Vector3D posRect, Vector3D posText) {
         std::shared_ptr<Entity> button = std::make_shared<Entity>();
-        button->addRenderer("", "./assets/menu/buttons.png", text, 0.0F, pos, 0.0F, rect, posRect, posText);
+        button->addRenderer("", "./assets/menu/buttons.png", text, 50, pos, 0.0F, rect, posRect, posText);
         _entities.find(DRAWABLE)->second.push_back(button);
         _entities.find(CLICKABLE)->second.push_back(button);
     }
