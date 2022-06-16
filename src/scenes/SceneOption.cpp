176357e30d9ec30e5ac08ec45e518fe9b6f46ec3
Ilan <ilan.chekroun@epitech.eu>
2022-06-16 15:30:07 +0200

    [EDIT] - Edit position sound text

--- src/scenes/SceneOption.cpp
+++ src/scenes/SceneOption.cpp
@@ -19,7 +19,7 @@ namespace indie {
         _entities.emplace(typeEntity::CLICKABLE, clicable_ent);
         Entities.createBackground(_entities, "./assets/options/dirt_background.png");
         Entities.createLogo(_entities, "./assets/options/settings.png", "Play", {650.0f, 150.0f, 0.0f}, {530.0f, 95.0f, 0.0f}, {0.0f, 0.0f, 0.0f});
-        Entities.createSlider(_entities, "Sound", {750.0f, 450.0f, 0.0f}, {300.0f, 40.0f, 0.0f}, {0.0f, 0.0f, 0.0f}, {890.0f, 470.0f, 0.0f});
+        Entities.createSlider(_entities, "SOUND", {750.0f, 450.0f, 0.0f}, {300.0f, 40.0f, 0.0f}, {0.0f, 0.0f, 0.0f}, {750.0f, 400.0f, 0.0f});
         Entities.createLogo(_entities, "./assets/options/slider.png", "Play", {750.0f, 450.0f, 0.0f}, {15.0f, 40.0f, 0.0f}, {300.0f, 0.0f, 0.0f});
         Entities.createButton(_entities, "FULLSCREEN", {550.0f, 650.0f, 0.0f}, {800.0f, 80.0f, 0.0f}, {0.0f, 0.0f, 0.0f}, {790.0f, 670.0f, 0.0f});
     }
