c6bb3ce0b96b3b4dc5431eda90ab6f142f327b6a
yoann <yoann.elasmar@epitech.eu>
2022-06-16 17:39:47 +0200

    [ADD] - Bomb display

---
+++ src/raylib/RShape.cpp
@@ -0,0 +1,28 @@
+/*
+** EPITECH PROJECT, 2022
+** Indie
+** File description:
+** RTexture3D
+*/
+
+#include "RShape.hpp"
+#include <iostream>
+
+namespace indie {
+    RShape::RShape(const std::string &filename)
+    {
+        _texture = LoadTexture(filename.c_str());
+    }
+
+    RShape::~RShape()
+    {
+        UnloadTexture(this->_texture);
+    }
+
+    void RShape::draw(const float &x, const float &y, Camera camera)
+    {
+        BeginMode3D(camera);
+        DrawCubeTexture(_texture, (Vector3){ 0.0f, 0.0f, 0.0f }, 50, 50, 50, WHITE);
+        EndMode3D();
+    }
+}
---
+++ src/raylib/RShape.hpp
@@ -0,0 +1,26 @@
+/*
+** EPITECH PROJECT, 2022
+** Indie
+** File description:
+** RShape
+*/
+
+#ifndef RSHAPE_HPP_
+    #define RSHAPE_HPP_
+
+    #include <string>
+    #include "raylib.h"
+
+    namespace indie {
+        class RShape {
+            public:
+                RShape(const std::string &filename);
+                ~RShape();
+
+                void draw(const float &x, const float &y, Camera camera);
+                Texture2D _texture;
+            private:
+        };
+    }
+
+#endif /* !RSHAPE_HPP_ */
