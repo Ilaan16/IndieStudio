45945e2588416af294c89fac4fd6f688d9c251cf
Nekomay <maylis.ducournau@epitech.eu>
2022-06-17 10:59:41 +0200

    [FIX] - removed lines not useful in config.sh

--- config.sh
+++ config.sh
@@ -3,8 +3,6 @@ if [ ! -d "$DIR" ]; then
   # Take action if $DIR exists. #
   echo "Installing config files in ${DIR}..."
   mkdir build
-  cmake -B ./build
-  make -C build/
 fi
   cmake -B ./build
   make -C build/
